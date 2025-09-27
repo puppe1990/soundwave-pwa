90eeb8157da019dbfecb287a3957b33b1285e6c9
puppe1990 <matheus.puppe90@hotmail.com>
2025-09-27 10:18:35 -0300

    Update dependencies and service worker configuration for PWA
    - Downgraded vite-plugin-pwa from version 1.0.3 to 0.20.5 in package.json and package-lock.json.
    - Adjusted workbox dependencies in vite.config.ts for compatibility.
    - Added new service worker files (registerSW.js and sw.js) for improved caching and offline support.
    - Enhanced service worker registration logic to ensure proper functionality in development mode.

--- vite.config.ts
+++ vite.config.ts
@@ -87,34 +87,10 @@ export default defineConfig(({ mode }) => ({
         dir: 'ltr'
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-              },
-              cacheKeyWillBeUsed: async ({ request }) => {
-                return `${request.url}?v=1`;
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'gstatic-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-              }
-            }
-          }
-        ]
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+      },
+      devOptions: {
+        enabled: true
       }
     })
   ].filter(Boolean),
